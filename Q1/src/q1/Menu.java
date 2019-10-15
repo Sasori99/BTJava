24ba10465e2395c47386d682f6741151e811fe16
OKAMI <qpham139@gmail.com>
2019-10-15 11:58:57 +0700

    Java

--- Q1/src/q1/Menu.java
+++ Q1/src/q1/Menu.java
@@ -9,7 +9,7 @@ public class Menu {
              System.out.println("1.Nhap so dien thoai");
              System.out.println("2.Tim so dien thoai theo vung");
              System.out.println("3.Hien thi danh sach");
-             System.out.println("4.Hien thi tong so dien theo tung vung ");
+             System.out.println("4.Hien thi tong so dien thoai theo tung vung ");
              System.out.println("5.Tim so dien thoai theo duoi");
              System.out.println("0.Exit");
              System.out.print("Moi chon: ");
@@ -22,6 +22,9 @@ public class Menu {
                      break;
                  }
                  case 2:{
+                     System.out.print("Nhap ma vung can tim kiem: ");
+                     int mavung = Integer.parseInt(in.nextLine());
+                     book.searchByArea(mavung);
                      break;
                  }
                  case 3:{
@@ -29,9 +32,18 @@ public class Menu {
                      break;
                  }
                  case 4:{
+                     book.demTheoVung();
                      break;
                  }
                  case 5:{
+                     String duoi;
+                     while (true){
+                         System.out.print("Nhap duoi can tim: ");
+                         duoi = in.nextLine();
+                         if (duoi.matches("\\d+")) break;
+                         System.out.println("Chi nhap cac so");
+                     }
+                     book.timKiemTheoDuoi(duoi);
                      break;
                  }
                  case 0: System.exit(0);
--- Q1/src/q1/PhoneBook.java
+++ Q1/src/q1/PhoneBook.java
@@ -1,7 +1,9 @@
 package q1;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class PhoneBook {
     ArrayList<PhoneNumber> list;
@@ -72,20 +74,73 @@ public class PhoneBook {
         int op;
         Scanner in = new Scanner(System.in);
         while (true){
-            System.out.println("1.Nhap so noi vung");
-            System.out.println("2.Nhap so quoc te");
+            System.out.println("1.Danh ba trong nuoc");
+            System.out.println("2.Danh ba quoc te");
+            System.out.println("3 danh ba ");
             System.out.print("Moi chon: ");
             op = Integer.parseInt(in.nextLine());
-            if (op == 1 || op == 2) break;
-            else System.out.println("Chi nhap 1 hoac 2");
+            if (op == 1 || op == 2 || op == 3) break;
+            else System.out.println("Chi nhap tu 1 den 3");
         }
+        if (op == 1) hienThiNV();
+        if (op == 2) hienThiQT();
+        if (op == 3) hienThiCaDS();
+    }
+    
+    public void hienThiCaDS(){
         System.out.println("=============Danh ba=============");
         for(PhoneNumber p : list)
             System.out.println(p.toString());
         System.out.println("=================================");
     }
     
+    public void hienThiNV(){
+        System.out.println("==========Danh ba noi vung==========");
+        for(PhoneNumber p : list)
+            if (!(p instanceof IntPhoneNumber))  System.out.println(p.toString());
+        System.out.println("====================================");
+    }
+    
+    public void hienThiQT(){
+        System.out.println("==========Danh ba quoc te===========");
+        for(PhoneNumber p : list)
+            if (p instanceof IntPhoneNumber)  System.out.println(p.toString());
+        System.out.println("====================================");
+    }
+    
     public void searchByArea(int area){
-        for(PhoneNumber p : list);
+        System.out.println("Danh sach dien thoai co ma vung " + area + " la:");
+        for(PhoneNumber p : list)
+            if (p.getMaVung() == area)  System.out.println(p.toString());
+    }
+    
+    private Integer[] phanLoaiVung(){
+        Set<Integer> ls = new HashSet<Integer>();
+        for(PhoneNumber p : list)
+            ls.add(p.getMaVung());
+        return ls.toArray(new Integer[ls.size()]);
+    }
+    
+    public void demTheoVung(){
+        Integer[] ls = phanLoaiVung();
+        for(Integer i:ls){
+            int cnt = 0;
+            for(PhoneNumber p : list)
+                if (p.getMaVung() == i) cnt++;
+            System.out.println("So luong so dien thoai co ma vung " + i + " la: " + cnt);
+        }
+    }
+    
+    public void timKiemTheoDuoi(String duoi){
+        System.out.println("Danh sach dien thoai co duoi " + duoi + " la: ");
+        boolean check = true;
+        for(PhoneNumber p : list) {
+            if (p.getSdt().contains(duoi)) {
+                System.out.println(p.toString());
+                check = false;
+            }
+        }
+        if (check)System.out.println("Khong ton tai!!!!");
     }
 }
+    
